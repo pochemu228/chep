2ab329a0e17742ce07ff493dd460d37d0d090e3e
pochemu228 <arinasmirnova096@gmail.com>
2025-02-25 10:25:56 +0900

    exs1.py

--- exs1.py
+++ exs1.py
@@ -1,47 +1,78 @@
-class Resource:
-    def __init__(self, name):
+class Character:
+    def __init__(self, attack_damage, attack_speed, name):
+        self._attack_damage = attack_damage
+        self._attack_speed = attack_speed
         self._name = name
 
-    def name(self):
+    def get_name(self):
         return self._name
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.name()})"
+    def get_attack_damage(self):
+        return self._attack_damage
+
+    def get_attack_speed(self):
+        return self._attack_speed
 
 
-class Stick(Resource):
-    pass
-
-
-class Stone(Resource):
-    pass
-
-
-class Sword:
-    def __init__(self, durability=100):
-        self._durability = durability
-
-    def durability(self):
-        return self._durability
-
-    def craft(self, stick, stones):
-        if isinstance(stick, Stick) and all(isinstance(s, Stone) for s in stones):
-            if len(stones) >= 2:
-                print(f"Crafted a {self.__class__.__name__}")
-                return self
-        else:
-            raise ValueError("Invalid ingredients")
+class StoneSword:
+    def __init__(self, attack_damage, attack_speed, name):
+        self._attack_damage = attack_damage
+        self._attack_speed = attack_speed
+        self._name = name
 
     def info(self):
-        return f"Sword with durability: {self.durability()}"
+        print(f"{self._name}, "
+              f"Класс оружия: Каменный меч, "
+              f"Физический урон: {self._attack_damage}, "
+              f"Быстрая атака: {self._attack_speed}")
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}(durability={self.durability()})"
+    def upgrade(self, new_attack_damage, new_attack_speed):
+        old_damage = self._attack_damage
+        old_speed = self._attack_speed
+
+        self._attack_damage = new_attack_damage
+        self._attack_speed = new_attack_speed
+
+        print(f"Меч был усовершенствован. "
+              f"Физический урон увеличился с {old_damage} до {new_attack_damage}, "
+              f"Быстрая атака увеличилась с {old_speed} до {new_attack_speed}")
 
 
-stick = Stick('Stick')
-stone1 = Stone('Stone')
-stone2 = Stone('Stone')
-sword = Sword()
-crafted_sword = sword.craft(stick, [stone1, stone2])
-print(crafted_sword.info())
+class Axe:
+    def __init__(self, damage, cutting_down_trees, name):
+        self._damage = damage
+        self._cutting_down_trees = cutting_down_trees
+        self._name = name
+
+    def get_damage(self):
+        return self._damage
+
+    def get_cutting_down_trees(self):
+        return self._cutting_down_trees
+
+    def info(self):
+        print(f"{self._name}, "
+              f"Класс оружия: Топор, "
+              f"Физический урон: {self._damage}, "
+              f"Вырубка деревьев: {self._cutting_down_trees}")
+
+    def upgrade(self, new_damage, new_cutting_down_trees):
+        old_damage = self._damage
+        old_cutting = self._cutting_down_trees
+
+        self._damage = new_damage
+        self._cutting_down_trees = new_cutting_down_trees
+
+        print(f"Топор был усовершенствован. "
+              f"Физический урон увеличился с {old_damage} до {new_damage}, "
+              f"Вырубка деревьев увеличилась с {old_cutting} до {new_cutting_down_trees}")
+
+
+sword = StoneSword(12, 1.2, "Клинок")
+sword.info()
+sword.upgrade(15, 1.5)
+axe = Axe(14, 7, "Лесоруб")
+axe.info()
+axe.upgrade(21, 9)
+
+
